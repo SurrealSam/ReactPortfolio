807bc3a59f8bc6519e8efa92f641327743fac133
Sammye Jones <sajones@ucdavis.edu>
2020-04-07 17:51:15 -0700

    added readme and some routers

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import MainPage from "./components/pages/main";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
     <div>
-    
-    <MainPage/>
-    
+      <Router>
+    <Route exact path="/" component={MainPage} />
+    </Router>
     </div>
   );
 }
